89eef77b5273e4dc58da1249c66d4ce14b3c4805
Davi Guerra Alves <davialves.war@gmail.com>
2025-05-03 15:39:18 -0300

    Add a message handler

--- bot.py
+++ bot.py
@@ -13,7 +13,6 @@ from importlib import reload
 load_dotenv()
 TELEGRAM_API_KEY = os.getenv('TELEGRAM_API_KEY')
 
-
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -30,6 +29,14 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
     await context.bot.send_photo(update.effective_chat.id,photo='https://thispersondoesnotexist.com/')
 
+async def save_resume(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f'{update.message.text}'
+    )
+
+
 
 def main():
     """Função principal que define o bot."""
@@ -42,8 +49,12 @@ def main():
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     echo_handler = CommandHandler('echo', echo)
 
+    save_resume_handler = CommandHandler('s', save_resume)
+    save_resume_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), save_resume)
+
     # Diferentes manipuladores para diferentes comandos
     application.add_handler(echo_handler)
+    application.add_handler(save_resume_handler)
 
     application.run_polling()
     
